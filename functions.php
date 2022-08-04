3fc2760092ddd2a85118abdbc1d5437951d3a65a
Nicolas Herbreteau <nicolasherbreteau@Nicolass-MacBook-Pro.local>
2022-08-04 12:58:17 +0100

    updated files after pull review

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
-function getAllCountriesFromDb():array{
+function getAllCountriesFromDb(): array
+{
     $connectionString = 'mysql:host=db; dbname=traveler';
     $dbUsername = 'root';
     $dbPassword = 'password';
     $db = new PDO($connectionString, $dbUsername, $dbPassword);
-
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $queryString = 'SELECT * FROM `country_cards`;';
     $query = $db->prepare($queryString);
@@ -16,16 +16,16 @@ function getAllCountriesFromDb():array{
 function displayCards(array $countries) :string {
     $countriesHTML = '';
     foreach ($countries as $country) {
-
-        if(!is_array($country) ||
+        if (
+            !is_array($country) ||
             !array_key_exists('name', $country) ||
             !array_key_exists ('place', $country) ||
             !array_key_exists ('landmark', $country) ||
             !array_key_exists ('activity', $country) ||
-            !array_key_exists ('food', $country)) {
+            !array_key_exists ('food', $country)
+        ) {
             return '';
         }
-
         $countriesHTML .=
             '<div class="countryCard">' .
             '<div class="name">'.
--- index.php
+++ index.php
@@ -1,28 +1,26 @@
 <?php require_once 'functions.php';
-$allResults= getAllCountriesFromDb();
+require_once 'form.php';
+
+$allResults = getAllCountriesFromDb();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700&family=Spectral+SC:wght@200;300;400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700&family=Spectral+SC:wght@200;300;400&display=swap"
+          rel="stylesheet">
     <title>Traveler</title>
 </head>
 <body>
-
     <nav>
         <h1>TRAVELER</h1>
     </nav>
-
-
-<div class="cardContainer">
-<?php
-echo displayCards($allResults);
-?>
-</div>
-
+    <div class="cardContainer">
+        <?php
+        echo displayCards($allResults);
+        ?>
+    </div>
 </body>
 </html>
